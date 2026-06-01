7d8da069a6da19e7feaf50c5e9c41e6b7d7efc31
Ayushi Patel <pate5752@sheridancollege.ca>
2026-06-01 12:19:16 -0400

    text file added

---
+++ GitIntro/src/main/java/com/mycompany/gitintro/GitIntro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gitintro;
+
+/**
+ *
+ * @author patel
+ */
+public class GitIntro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
